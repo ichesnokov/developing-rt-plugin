7b33f4cfbcdba424f1e4d4c6894fcc15f8220d5f
Ilya Chesnokov <me@ichesnokov.ru>
2017-08-02 01:59:19 +0300

    Infastructure for the presentation

---
+++ install.sh
@@ -0,0 +1,13 @@
+if `which git cpanm > /dev/null` ; then
+    echo "Installing App::revealup"
+else
+    echo "You need git and cpanm configured, exiting now"
+    exit 1;
+fi
+
+# Install old version from cpan along with deps
+cpanm App::revealup
+
+# Clone repo and install 
+git clone https://github.com/yusukebe/App-revealup
+cpanm --notest ./App-revealup
